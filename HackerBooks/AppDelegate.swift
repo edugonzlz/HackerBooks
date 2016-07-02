20b3964fa378228e03983cc2d1d8f0276b6cc0db
Eduardo González <fonologico@gmail.com>
2016-07-02 18:33:37 +0200

    Realizada primera prueba de modelo con json local. Parece que funciona

--- HackerBooks/AppDelegate.swift
+++ HackerBooks/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let json = try? loadAndSerialize(fromLocalJSONFile: "books_readable.json")
+
+        let books = booksArray(fromJSONArray: json!)
+
+        let model = Library(withBooks: books)
+
         return true
     }
 
